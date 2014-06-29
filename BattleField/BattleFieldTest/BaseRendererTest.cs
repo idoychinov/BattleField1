d799a134ca21d395ebe72699e7e9559017484c2b
delyan-nikolov-1992 <delyan.nikolov.1992@gmail.com>
2014-06-29 12:24:30 +0300

    Adding empty unit test classes for every non-empty class in the main solution

---
+++ BattleField/BattleFieldTest/BaseRendererTest.cs
@@ -0,0 +1,14 @@
+﻿namespace BattleFieldTest
+{
+    using BattleField;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class BaseRendererTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ BattleField/BattleFieldTest/BattleFieldGameTest.cs
@@ -0,0 +1,14 @@
+﻿namespace BattleFieldTest
+{
+    using BattleField;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class BattleFieldGameTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
