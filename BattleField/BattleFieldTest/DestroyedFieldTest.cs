875f4a91ea10a2264c3158729805d61a2260d073
delyan-nikolov-1992 <delyan.nikolov.1992@gmail.com>
2014-07-01 13:41:31 +0300

    Added more empty test classes

---
+++ BattleField/BattleFieldTest/DestroyedFieldTest.cs
@@ -0,0 +1,14 @@
+﻿namespace BattleFieldTest
+{
+    using BattleField;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class DestroyedFieldTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ BattleField/BattleFieldTest/PositionTest.cs
@@ -0,0 +1,14 @@
+﻿namespace BattleFieldTest
+{
+    using BattleField;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class PositionTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
