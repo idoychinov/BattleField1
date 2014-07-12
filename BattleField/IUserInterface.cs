4c3fbe7d8732167bcd112ae0e3f4f972296d77e3
Stefan Yordanov <stefanyordanov324@gmail.com>
2014-07-12 18:53:39 +0300

    Second try to commit

--- BattleField/IUserInterface.cs
+++ BattleField/IUserInterface.cs
@@ -4,9 +4,6 @@ namespace BattleField
     public interface IUserInterface
     {
         int ReadInteger();
-<<<<<<< HEAD
         IEnumerable<int> ReadMultipleIntegers(int count);
-=======
->>>>>>> origin/master
     }
 }
