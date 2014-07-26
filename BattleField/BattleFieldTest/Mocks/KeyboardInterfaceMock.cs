76f8236ddf760b05803ddfde743d827c3a204ddd
Stefan Yordanov <stefanyordanov324@gmail.com>
2014-07-27 01:03:09 +0300

    Mocking the UI

---
+++ BattleField/BattleFieldTest/Mocks/KeyboardInterfaceMock.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleFieldTest.Mocks
+{
+    using BattleField.Interfaces;
+    public abstract class KeyboardInterfaceMock : IKeyBoardInterfaceMock
+    {
+        private IEnumerable<int> singleIntegers;
+        private IEnumerator<int> currentSingleElement;
+
+        private IEnumerable<IEnumerable<int>> multipleIntegers;
+        private IEnumerator<IEnumerable<int>> currentMultipleElements;
+
+        public KeyboardInterfaceMock(IEnumerable<int> singleIntegers, IEnumerable<IEnumerable<int>> multipleIntegers)
+        {
+            this.singleIntegers = singleIntegers;
+            this.multipleIntegers = multipleIntegers;
+            this.currentSingleElement = singleIntegers.GetEnumerator();
+            this.currentMultipleElements = multipleIntegers.GetEnumerator();
+            this.GenerateUIMock();
+        }
+
+        public int GetNextSingleInteger()
+        {
+            if(!this.currentSingleElement.MoveNext())
+            {
+                throw new IndexOutOfRangeException("End of array reached.");
+            }
+
+            return this.currentSingleElement.Current;
+        }
+
+        public IEnumerable<int> GetNextMultileIntegers()
+        {
+            if (!this.currentMultipleElements.MoveNext())
+            {
+                throw new IndexOutOfRangeException("End of array reached.");
+            }
+
+            return this.currentMultipleElements.Current;
+        }
+
+        public IUserInterface UI { get; protected set; }
+
+        public abstract void GenerateUIMock();
+    }
+}
---
+++ BattleField/BattleFieldTest/Mocks/MoqKeyboardInterfaceMock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+namespace BattleFieldTest.Mocks
+{
+    using BattleField.Interfaces;
+    class MoqKeyboardInterfaceMock : KeyboardInterfaceMock, IKeyBoardInterfaceMock
+    {
+        public override void GenerateUIMock()
+        {
+            var mock = new Mock<IUserInterface>();
+            mock.Setup(ui => ui.ReadInteger()).Returns(this.GetNextSingleInteger());
+            mock.Setup(ui => ui.ReadMultipleIntegers(It.IsAny<int>())).Returns(this.GetNextMultileIntegers());
+            this.UI = mock.Object;
+        }
+    }
+}
