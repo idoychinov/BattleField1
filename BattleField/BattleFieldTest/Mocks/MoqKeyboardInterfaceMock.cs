04464ee32b5bb9f1ec84c2fd0d28269bf0a78e9b
Stefan Yordanov <stefanyordanov324@gmail.com>
2014-07-27 11:03:17 +0300

    Position Tests Added
    2 of them fail. Validate the data for the position

--- BattleField/BattleFieldTest/Mocks/MoqKeyboardInterfaceMock.cs
+++ BattleField/BattleFieldTest/Mocks/MoqKeyboardInterfaceMock.cs
@@ -9,6 +9,13 @@ namespace BattleFieldTest.Mocks
     using BattleField.Interfaces;
     class MoqKeyboardInterfaceMock : KeyboardInterfaceMock, IKeyBoardInterfaceMock
     {
+        public MoqKeyboardInterfaceMock(IEnumerable<int> singleIntegers, IEnumerable<IEnumerable<int>> multipleIntegers)
+            : base(singleIntegers, multipleIntegers)
+        {
+
+        }
+
+
         public override void GenerateUIMock()
         {
             var mock = new Mock<IUserInterface>();
---
+++ BattleField/BattleFieldTest/Mocks/MoqRendererMock.cs
@@ -0,0 +1,21 @@
+﻿using BattleField.Interfaces;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleFieldTest.Mocks
+{
+    class MoqRendererMock : RendererMock, IRendererMock
+    {
+        protected override void GenerateRendererMock()
+        {
+            var rendererMock = new Mock<IRenderer>();
+            rendererMock.Setup(r => r.DrawGameField(It.IsAny<IGameField>())).Verifiable();
+            rendererMock.Setup(r => r.PrintMessage(It.IsAny<string>())).Verifiable();
+            this.Renderer = rendererMock.Object;
+        }
+    }
+}
